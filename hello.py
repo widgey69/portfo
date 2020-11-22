6205fb4a2703a7d86c045ff419e0b08e017826b9
widgey69 <66630004+widgey69@users.noreply.github.com>
2020-11-22 19:28:20 +0000

    first

---
+++ hello.py
@@ -0,0 +1,49 @@
+from flask import Flask,render_template, url_for, request, redirect
+app = Flask(__name__)
+import csv
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        name = data["name"]
+        message = data["message"]
+        file = database.write(f'\n{email}, {name}, {message}')
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data["email"]
+        name = data["name"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, name, message])
+
+
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('./thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return ' something went wrong'
+
+
+
+
+
+
